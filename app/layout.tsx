8d2bc45baaa71f819629eced2cea6f067edc177c
Dhunu <angelsaikia333@gmail.com>
2024-05-19 11:55:07 +0530

    metadata fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import NextTopLoader from "nextjs-toploader";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+    metadataBase: new URL("https://blogify-stories.vercel.app/"),
     title: {
         default: "Blogify - A place for your stories",
         template: "%s | Blogify",
