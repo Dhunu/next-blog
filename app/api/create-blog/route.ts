762e176afeb3c228292c847617425da82be64eaa
Dhunu <angelsaikia333@gmail.com>
2024-05-19 18:08:14 +0530

    create-blog route fix

--- app/api/create-blog/route.ts
+++ app/api/create-blog/route.ts
@@ -5,17 +5,15 @@ export async function POST(req: NextRequest) {
     const { title, slug, content } = await req.json();
 
     if ([title, slug, content].some((field) => field?.trim() === "")) {
-        return {
-            status: 400,
-            json: {
-                message: "Please fill in all fields",
-            },
-        };
+        return NextResponse.json(
+            { message: "All fields are required" },
+            { status: 400 }
+        );
     }
 
     const res = await createBlog({ title, slug, content });
 
     const { blog } = await res.json();
 
-    return NextResponse.json(blog, { status: 200 });
+    return NextResponse.json({ blog }, { status: 200 });
 }
